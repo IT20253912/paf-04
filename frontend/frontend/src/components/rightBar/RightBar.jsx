a6bc7ac2d71abd73e75488ad429b6cee45baebda
chathudu <88680508+IT20253912@users.noreply.github.com>
2024-05-11 21:06:55 +0530

    user mnagement

--- frontend/frontend/src/components/rightBar/RightBar.jsx
+++ frontend/frontend/src/components/rightBar/RightBar.jsx
@@ -1,64 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../css/rightBar.css";
+import axios from "axios";
+
 function RightBar() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5005/api/users/all");
+        setUsers(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="rightBar">
       <div className="container">
-       
-
         <div className="item">
           <span>Online Friends</span>
-          <div className="user">
-            <div className="userInfo">
-              <img
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="online" />
-              <span>Saman Kumara</span>
+          {users.map((user) => (
+            <div className="user" key={user.id}>
+              <div className={`userInfo ${user.online ? "online" : ""}`}>
+                <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <span>{user.name}</span>
+              </div>
             </div>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="online" />
-              <span>Sandeep Saman</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="online" />
-              <span>Namal Namal</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="online" />
-              <span>Test user</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="userInfo">
-              <img
-                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <div className="online" />
-              <span>Saman Saman</span>
-            </div>
-          </div>
-
+          ))}
         </div>
       </div>
     </div>
